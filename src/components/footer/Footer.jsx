b4341b36081f5f5e04a8b762936caf0cb98c5d2a
Daniele Ferrari <eleinadinha@hotmail.com>
2020-12-26 21:28:22 -0300

    Criando o footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './footer.scss';
 
+import facebook from '../../assets/icons/facebook.svg';
+import instagram from '../../assets/icons/instagram.svg';
+import email from '../../assets/icons/email.svg';
+import heart from '../../assets/icons/heart.svg';
+
 function Footer () {
     return (
         <div className="footer">
-            <h2>Footer</h2>
+            <div>
+                <img src={facebook} alt="Logo do Facebook"/>
+                <p>/aline.ferrari.9887</p>
+            </div>
+
+            <div>
+                <img src={instagram} alt="Logo do Instagram"/>
+                <p>@alineferrari8</p>
+            </div>
+
+            <div>
+                <img src={email} alt="Icone de e-mail"/>
+                <p>alinecri@yahoo.com.br</p>
+            </div>
+
+            <div className="author">
+                <p>Feito por Dani com</p>
+                <img src={heart} alt="Icone de coração"/>
+            </div>
+
         </div>
     );
 }
