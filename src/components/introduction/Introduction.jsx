9ecd5027253f96df251a4d5c6642863a0c187c67
Daniele Ferrari <eleinadinha@hotmail.com>
2020-12-26 22:45:31 -0300

    Adicionando apresentacao

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './introduction.scss';
 
+import aline1 from '../../assets/images/aline1.png';
+
 function Introduction () {
     return (
         <div className="Introduction">
-            <h2>INTRODUÇÃO</h2>
+            <h2>Dra. Aline Ferrari</h2>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+            </div>
+
+            <img src={aline1} alt="Foto de Aline"/>
         </div>
     );
 }
