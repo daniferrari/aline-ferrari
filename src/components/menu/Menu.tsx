32ffdf169a75d10678f35cd6a0e81cf0f431c7b7
Daniele Ferrari <eleinadinha@hotmail.com>
2020-12-28 21:56:54 -0300

    Adicionando menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -4,14 +4,43 @@ import './menu.scss';
 import logo from '../../assets/images/logo.png';
 import sanduiche from '../../assets/icons/menu.svg';
 
-function Menu() {
-  return (
-    <div className="Menu">
-      <h1>Dra. Aline</h1>
-      <img src={logo} alt="Logo" />
-      <img className="sanduiche" src={sanduiche} alt="Icone do menu"/>
-    </div>
-  );
+class Menu extends React.Component<{}, { openMenu: boolean }> {
+
+  constructor (props: any) {
+    super(props)
+    this.state = { openMenu: true };
+
+    this.click = this.click.bind(this);
+  }
+
+
+  click = () => {
+    this.setState(state => ({
+      openMenu: !state.openMenu
+    }))
+  }
+
+  render () {
+    return (
+      <div className="Menu">
+        <div className="menuHeader">
+          <h1>Dra. Aline</h1>
+          <img src={logo} alt="Logo" />
+          <img onClick={this.click} className="sanduiche" src={sanduiche} alt="Icone do menu"/>
+        </div>
+  
+         { this.state.openMenu ? <div className="menuContent">
+           <ul>
+             <li>Sobre</li>
+             <li>Especialidades</li>
+             <li>Na Mídia</li>
+             <li>Marque sua consulta</li>
+             <li>Entre em contato</li>
+           </ul>
+        </div> : null  }
+      </div>
+    )
+  }
 }
 
 export default Menu;
