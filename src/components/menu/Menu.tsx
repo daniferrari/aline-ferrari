baaf409b83dc338d9420d0b69d8b41a6e17feae5
Daniele Ferrari <eleinadinha@hotmail.com>
2020-12-29 21:34:33 -0300

    Arrumando disposicao do menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -23,21 +23,23 @@ class Menu extends React.Component<{}, { openMenu: boolean }> {
   render () {
     return (
       <div className="Menu">
-        <div className="menuHeader">
-          <h1>Dra. Aline</h1>
-          <img src={logo} alt="Logo" />
-          <img onClick={this.click} className="sanduiche" src={sanduiche} alt="Icone do menu"/>
+        <div className="menuContainer">
+          <div className="menuHeader">
+            <h1>Dra. Aline</h1>
+            <img src={logo} alt="Logo" />
+            <img onClick={this.click} className="sanduiche" src={sanduiche} alt="Icone do menu"/>
+          </div>
+    
+          { this.state.openMenu ? <div className="menuContent">
+            <ul>
+              <li>Sobre</li>
+              <li>Especialidades</li>
+              <li>Na Mídia</li>
+              <li>Marque sua consulta</li>
+              <li>Entre em contato</li>
+            </ul>
+          </div> : null  }
         </div>
-  
-         { this.state.openMenu ? <div className="menuContent">
-           <ul>
-             <li>Sobre</li>
-             <li>Especialidades</li>
-             <li>Na Mídia</li>
-             <li>Marque sua consulta</li>
-             <li>Entre em contato</li>
-           </ul>
-        </div> : null  }
       </div>
     )
   }
