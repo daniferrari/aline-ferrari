926814967ddabf1a2b3698c8e878c824ec8827e4
Daniele Ferrari <eleinadinha@hotmail.com>
2021-01-02 17:58:21 -0300

    Arrumando o size do carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -16,29 +16,31 @@ function CarouselFunction () {
     };
 
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect} className="carouselContainer">
-            <Carousel.Item>
-                <img src={aline1} alt="Foto de Aline"/>
-                <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={aline1} alt="Foto de Aline"/>
-                <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={aline1} alt="Foto de Aline"/>
-                <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+        <div className="carouselIntroduction">
+            <Carousel activeIndex={index} onSelect={handleSelect} className="carouselContainer">
+                <Carousel.Item>
+                    <img src={aline1} alt="Foto de Aline"/>
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={aline1} alt="Foto de Aline"/>
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={aline1} alt="Foto de Aline"/>
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
     )
 }
 
