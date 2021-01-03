c55432c2f18e69727539ff1bead1ef01663b209d
Daniele Ferrari <eleinadinha@hotmail.com>
2021-01-02 22:37:07 -0300

    Adicionando texto no slide

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -20,26 +20,33 @@ function CarouselFunction () {
         <div className="carouselIntroduction">
             <Carousel activeIndex={index} onSelect={handleSelect} className="carouselContainer">
                 <Carousel.Item>
-                    <img src={aline2} alt="Foto de Aline"/>
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+
+                    <div className="carouselItem">
+                        <img src={aline2} alt="Foto de Aline"/>
+
+                        <div className="carouselInfo">
+                            <h5>TESTE ESCRIOT</h5>
+                            <p>AHSUAHSUHASUHAUSHAUSHUASHUASHUAHSUAHS</p>
+                            <p>AHSUAHSUHASUHAUSHAUSHUASHUASHUAHSUAHS</p>
+                        </div>
+                    </div>
+                    
                 </Carousel.Item>
+
                 <Carousel.Item>
-                    <img src={aline1} alt="Foto de Aline"/>
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+
+                    <div className="carouselItem">
+                        <img src={aline1} alt="Foto de Aline"/>
+
+                        <div className="carouselInfo">
+                            <h5>TESTE ESCRIOT</h5>
+                            <p>AHSUAHSUHASUHAUSHAUSHUASHUASHUAHSUAHS</p>
+                            <p>AHSUAHSUHASUHAUSHAUSHUASHUASHUAHSUAHS</p>
+                        </div>
+                    </div>
+
                 </Carousel.Item>
-                <Carousel.Item>
-                    <img src={aline1} alt="Foto de Aline"/>
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+
             </Carousel>
         </div>
     )
